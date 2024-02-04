7ae70585498e6f3c560b3fbc9cb277e4e652d901
vVoitovych <viktor.a.voitovych@gmail.com>
2024-02-04 14:16:03 +0200

    Add file to previous commit

---
+++ VulkanEngine/Renderer/RenderPasses/GuiPass.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "ECS/Entity.h"
+
+#include <memory>
+#include <vector>
+#include <string>
+
+namespace Renderer
+{
+	class RenderContext;
+	class RenderPass;
+	class DescriptorPool;
+	class FrameBuffer;
+
+	class GuiPass
+	{
+	public:
+		GuiPass(const std::unique_ptr<RenderContext>& context);
+		~GuiPass();
+
+		void Render(std::unique_ptr<RenderContext>& context);
+		void Cleanup(const std::unique_ptr<RenderContext>& context);
+
+		void ResizeResources(const std::unique_ptr<RenderContext>& context);
+	public:
+		static bool IsCursorPositionInGUI();
+
+	private:
+		void DrawGui(const std::unique_ptr<RenderContext>& context);
+		void DrawInspector(const std::unique_ptr<RenderContext>& context);
+		void DrawScene(const std::unique_ptr<RenderContext>& context);
+		void DrawHierarchyNode(const std::unique_ptr<RenderContext>& context, ECS::Entity entity, int& index);
+
+		void UploadFonts();
+
+	private:
+		std::unique_ptr<RenderPass> mRenderPass;
+		std::vector<FrameBuffer> mFrameBuffers;
+		std::unique_ptr<DescriptorPool> mDescriptorPool;
+
+	};
+}
+
+
