80c9aca3f3af633a1d90c02c90b3bd1984846920
vVoitovych <viktor.a.voitovych@gmail.com>
2024-03-15 14:57:03 +0200

    Add render component

---
+++ Scene/SceneSystems/RenderSystem.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ECS/System.h"
+#include "ECS/Coordinator.h"
+
+
+namespace Scene
+{
+	class RenderSystem : public ECS::System
+	{
+	public:
+
+
+	private:
+
+	};
+
+
+}
+
+
