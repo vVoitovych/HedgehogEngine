e6661576d81921b232bd6c761d995e11f5b15434
vVoitovych <viktor.a.voitovych@gmail.com>
2024-09-10 18:31:19 +0300

    added imgui sub module

--- ThirdParty/ImGui/Build-ImGui.lua
+++ ThirdParty/ImGui/Build-ImGui.lua
@@ -5,12 +5,35 @@ project "ImGui"
    targetdir "Binaries/%{cfg.buildcfg}"
    staticruntime "off"
 
-   files { "**.h", "**.cpp" }
+    IncludeDir = {}
+    IncludeDir["GLFW"] = "../glfw/glfw/include"
+    IncludeDir["Vulkan"] = VulkanSDK .. "/Include"
+
+   files 
+   { 
+    "imgui/imconfig.h", 
+    "imgui/imgui.cpp",
+    "imgui/imgui.h",
+    "imgui/imgui_demo.cpp",
+    "imgui/imgui_draw.cpp",
+    "imgui/imgui_internal.h",
+    "imgui/imgui_tables.cpp",
+    "imgui/imgui_widgets.cpp",
+    "imgui/imstb_rectpack.h",
+    "imgui/imstb_textedit.h",
+    "imgui/imstb_truetype.h",
+    "imgui/backends/imgui_impl_glfw.h",
+    "imgui/backends/imgui_impl_glfw.cpp",
+    "imgui/backends/imgui_impl_vulkan.h",
+    "imgui/backends/imgui_impl_vulkan.cpp" 
+    }
 
    includedirs
    { 
-   "../glfw/include"  
-    }
+    "%{IncludeDir.GLFW}",
+    "%{IncludeDir.Vulkan}",
+    "imgui" 
+   }
 
    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
